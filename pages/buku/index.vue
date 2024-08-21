136bc2bb8747278c19fe85fe79d6f7fd10e64190
WldnNgra123 <158022052+WldnNgra123@users.noreply.github.com>
2024-08-21 10:50:23 +0700

    Add files via upload

---
+++ pages/buku/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container-fluid">
+    <div class="row"> 
+      <div class="col-lg-12">
+        <div class="my-3">
+          <form @submit.prevent="getBuku">
+          <input
+            v-model="keyword"
+            type="search"
+            class="form-control rounded-5"
+            placeholder="mau baca apa hari ini?"
+            />
+          </form>
+        </div>
+        <div class="my-3 text-muted">menampilkan 4 dari 125</div>
+        <div class="row">
+          <div v-for="(book, i) in books" :key="i" class="col-lg-2">
+            <nuxt-link :to="`/buku/${book.id}`">
+              <div class="card mb-3">
+                <div class="card-body">
+                  <img :src="book.cover" class="cover" alt="cover" />
+                </div>
+              </div>
+            </nuxt-link>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    <nuxt-link to="/">
+      <button type="submit" class="btn btn-light btn-lg rounded-5 px-5">kembali</button></nuxt-link>
+</template>
+
+<script setup>
+const supabase = useSupabaseClient()
+const keyword = ref("")
+const books = ref([])
+const getBuku = async () => {
+  const { data, error} = await supabase.from('buku').select('* kategori(*)')
+  if(data) books.value= data
+
+}
+onMounted(() => {
+  getBuku()
+})
+</script>
+<style scoped>
+.card-body {
+  width: 100%;
+  height: 30em;
+  padding: 0;
+}
+.cover {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: 0 30;
+}
+.form-control {
+  background-color: #D9D9D9;
+}
+.btn {
+  background-color: #D9D9D9;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+nn<template>
+  <div class="container-fluid">
+    <div class="row my-5">
+      <div class="col-lg-6">
+        <nuxt-link to="/pengunjung/tambah">
+          <div class="card bg-pengunjung rounded-5">
+            <div class="card-body">
+              <h2>Pengunjung</h2>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+      <div class="col-lg-6">
+        <nuxt-link to="/buku">
+          <div class="card bg-buku rounded-5">
+            <div class="card-body">
+              <h2>Cari Buku</h2>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+  <h1 class="statistik">STATISTIK</h1>
+  <div class="container-fluid">
+    <div class="row my-5">
+      <div class="col-lg-6">
+        <nuxt-link to="/pengunjung">
+          <div class="card bg-light rounded-5">
+            <div class="card-body">
+              <h2> Pengunjung</h2>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+    
+
+      <div class="col-lg-6">
+          <div class="card bg-light rounded-5">
+            <div class="card-body">
+              <h2>Stock 3 Buku</h2>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  height: 250px;
+  box-shadow: 1px 1px 1px #696969;
+}
+.card.bg-pengunjung {
+  background-image: url('~/assets/bg-home-kunjungan (2).jpeg');
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+}
+.card.bg-buku {
+  background-image: url('~/assets/bg-home-cari-buku.jpg');
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;      
+}
+.statistik{
+  left: reight;
+}
+</style>
